b29b9f0a3722bb0b45391312a2eb584250207643
XLuma <llaplant@student.42quebec.com>
2026-05-06 18:32:12 -0400

    add some more styling for front page

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
-	import { m } from '$lib/paraglide/messages';
-	import { getLocale, setLocale } from '$lib/paraglide/runtime.js';
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+  import { m } from '$lib/paraglide/messages';
+  import { getLocale, setLocale } from '$lib/paraglide/runtime.js';
+  import './layout.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import header from '$lib/assets/header_cover.png';
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-	<h1>Snake</h1>
-<nav>
-<a href="/arena">{m["nav.arena"]()}</a>
-	<a href="/quests">{m["nav.quests"]()}</a>
-	<a href="/encyclopedia">{m["nav.encyclopedia"]()}</a>
-	<a href="/cards">{m["nav.cards"]()}</a>
-	<a href="/tournament">{m["nav.tournament"]()}</a>
-	<a href="/ranking">{m["nav.ranking"]()}</a>
-	<a href="/forum">{m["nav.forum"]()}</a>
+<h1
+  style="background: url({header}) center bottom / 900px 100px no-repeat;"
+  class="relative mx-auto mt-8 -mb-4.5 box-border block h-25 max-width-900 text-center text-transparent img-rendering-pixelated"
+>
+  Snake
+</h1>
+<nav
+  class="mx-auto my-0 nav-main-borders box-border flex h-9.5 max-width-900 justify-between overflow-auto overflow-hidden px-3.5 py-0"
+>
+  <a href="/arena">{m['nav.arena']()}</a>
+  <a href="/quests">{m['nav.quests']()}</a>
+  <a href="/encyclopedia">{m['nav.encyclopedia']()}</a>
+  <a href="/cards">{m['nav.cards']()}</a>
+  <a href="/tournament">{m['nav.tournament']()}</a>
+  <a href="/ranking">{m['nav.ranking']()}</a>
+  <a href="/forum">{m['nav.forum']()}</a>
 </nav>
-{@render children()}
+<main class="mx-auto mt-2.25 mb-0 nav-main-borders block max-w-[min(900px,90vw)]">
+  {@render children()}
+</main>
