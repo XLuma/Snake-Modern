6123a1a27224132a3473ac80af6182feedb73e4e
XLuma <llaplant@student.42quebec.com>
2026-05-19 18:39:13 -0400

    add new font, make layout for labels for stats

--- client/src/routes/encyclopedia/+page.svelte
+++ client/src/routes/encyclopedia/+page.svelte
@@ -5,6 +5,7 @@
   import { Application, AssetsLoader, Container, Sprite, Text } from 'svelte-pixi';
   import FruitEntryBg from '$lib/assets/fruit_entry_bg.png';
   import NokiaCFFont from '$lib/fonts/nokiafc22.ttf';
+  import _04b03Font from '$lib/fonts/04b03.ttf';
   import fruitBundle from '$lib/data/fruits/assetBundle';
 
   const initPromise = PIXI.Assets.init({
@@ -47,12 +48,12 @@
 <div class="relative mx-auto my-0 block h-106 w-220 bg-red-500">
   {#await initPromise then}
     <Application width={880} height={424} autoDensity={true} backgroundColor={0xffffff}>
-      <AssetsLoader assets={[FruitEntryBg, NokiaCFFont]} />
-      <Container>
+      <AssetsLoader assets={[FruitEntryBg, NokiaCFFont, _04b03Font]} />
+      <Container label="fruit_entry_container">
         <Container x={0} y={0}>
           <FruitEntry />
           <Text
-            text="hello"
+            text="pomme shampoing"
             label="fruit_name"
             x={4}
             y={2}
@@ -64,7 +65,12 @@
             x={413}
             y={28}
             anchor={0.5}
-            style={{ align: 'center', fontSize: 16, fontFamily: 'nokiafc22', fill: 'white' }}
+            style={{
+              align: 'center',
+              fontSize: 16,
+              fontFamily: 'nokiafc22',
+              fill: 'white'
+            }}
           />
           <Sprite
             label="fruit_sprite_shadow"
@@ -81,6 +87,42 @@
             x={18}
             y={31}
           />
+          <Container x={80} y={18} label="fruit_data_label_container">
+            <Text
+              text="score :"
+              label="fruit_score"
+              style={{ align: 'center', fontSize: 16, fontFamily: '04b03', fill: '#c7ff77' }}
+            />
+            <Text
+              text="vitamine :"
+              y={14}
+              label="fruit_vitamin"
+              style={{
+                align: 'center',
+                fontSize: 16,
+                fontFamily: '04b03',
+                fill: '#c7ff77'
+              }}
+            />
+            <Text
+              text="nutrition :"
+              y={28}
+              label="fruit_nutrition"
+              style={{ align: 'center', fontSize: 16, fontFamily: '04b03', fill: '#c7ff77' }}
+            />
+            <Text
+              text="conservation :"
+              y={42}
+              label="fruit_conservation"
+              style={{ align: 'center', fontSize: 16, fontFamily: '04b03', fill: '#c7ff77' }}
+            />
+            <Text
+              text="rouge,feuille,sucre,pomme"
+              y={59}
+              label="fruit_tags"
+              style={{ align: 'center', fontSize: 16, fontFamily: '04b03', fill: '#319202' }}
+            />
+          </Container>
         </Container>
       </Container>
       <Container x={442}>
